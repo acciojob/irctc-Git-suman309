1e3d74533da4c1fc314855466e560fe6f7360e8b
SUMAN SAURABH <110984246+Git-suman309@users.noreply.github.com>
2023-04-04 20:30:21 +0530

    Update PassengerRepository.java

--- src/main/java/com/driver/repository/PassengerRepository.java
+++ src/main/java/com/driver/repository/PassengerRepository.java
@@ -4,5 +4,6 @@ import com.driver.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PassengerRepository extends JpaRepository<Passenger,Integer> {
+//   m
 
 }
